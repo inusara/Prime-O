d15d3e6a77e56c997a0070a0abe9b6cadb643842
inusara <timvazquez@gmail.com>
2014-06-30 02:26:51 +0800

    added Controller and Motor function

--- Sketch/PrimeO/PrimeO.ino
+++ Sketch/PrimeO/PrimeO.ino
@@ -16,3 +16,37 @@ void loop() {
   digitalWrite(M2, HIGH);
   delay(30);
 }
+
+void Motor(int E, int M, int pwm_speed, boolean forward) {
+  analogWrite(E, pwm_speed);
+  if(forward) {
+    digitalWrite(M, HIGH);  
+  } else {
+    digitalWrite(M, LOW);  
+  }
+}
+
+void Controls(char signal, int pwm_speed) {
+  switch(signal) {
+    case 'f': //to move forward
+      Motor(E1, M1, pwm_speed, true);
+      Motor(E2, M2, pwm_speed, true);
+      break;   
+    case 'b': //to move backwards
+      Motor(E1, M1, pwm_speed, false);
+      Motor(E2, M2, pwm_speed, false);
+      break;     
+    case 'l': //to turn left
+      Motor(E1, M1, pwm_speed, false);
+      Motor(E2, M2, pwm_speed, true);
+      break;   
+    case 'r': //to turn right
+      Motor(E1, M1, pwm_speed, true);
+      Motor(E2, M2, pwm_speed, false);
+      break;  
+    case 's': //full stop
+      Motor(E1, M1, 0, false);
+      Motor(E2, M2, 0, false);
+      break;  
+  }
+}
