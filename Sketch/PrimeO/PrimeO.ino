4d3d8fb93a59050c0903f28ec287fa2ddcb5a043
inusara <timvazquez@gmail.com>
2014-06-30 05:22:48 +0800

    updated pin assignment to correct left right, updated function name, added time delay in Controller function then added test movement simulation using the Controller function - forward, back, left, right and stop

--- Sketch/PrimeO/PrimeO.ino
+++ Sketch/PrimeO/PrimeO.ino
@@ -1,20 +1,47 @@
-int E1 = 5,
-    M1 = 4,
-    E2 = 6,
-    M2 = 7;
+int E1 = 6,
+    M1 = 7,
+    E2 = 5,
+    M2 = 4;
 
 void setup() {
-    pinMode(M1, OUTPUT);
-    pinMode(M2, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
 }
 
 void loop() {
-  analogWrite(E1, 255);
-  analogWrite(E2, 255);
-  
-  digitalWrite(M1, HIGH);
-  digitalWrite(M2, HIGH);
-  delay(30);
+  //test - movement simulation - forward, back, left, right, stop
+  Controller('f', 255, 3000);
+  Controller('b', 255, 3000);
+  Controller('l', 255, 2000);
+  Controller('s', 0, 900);
+  Controller('r', 255, 2000);
+  Controller('s', 0, 3000);
+}
+
+void Controller(char signal, int pwm_speed, int time_delay) {
+  switch(signal) {
+    case 'f': //move forward
+      Motor(E1, M1, pwm_speed, true);
+      Motor(E2, M2, pwm_speed, true);
+      break;   
+    case 'b': //move backwards
+      Motor(E1, M1, pwm_speed, false);
+      Motor(E2, M2, pwm_speed, false);
+      break;     
+    case 'l': //turn left
+      Motor(E1, M1, 0, false);
+      Motor(E2, M2, pwm_speed, true);
+      break;   
+    case 'r': //turn right
+      Motor(E1, M1, pwm_speed, true);
+      Motor(E2, M2, 0, false);
+      break;  
+    case 's': //full stop
+      Motor(E1, M1, 0, false);
+      Motor(E2, M2, 0, false);
+      break;  
+  }
+  delay(time_delay);
 }
 
 void Motor(int E, int M, int pwm_speed, boolean forward) {
@@ -23,30 +50,5 @@ void Motor(int E, int M, int pwm_speed, boolean forward) {
     digitalWrite(M, HIGH);  
   } else {
     digitalWrite(M, LOW);  
-  }
-}
-
-void Controls(char signal, int pwm_speed) {
-  switch(signal) {
-    case 'f': //to move forward
-      Motor(E1, M1, pwm_speed, true);
-      Motor(E2, M2, pwm_speed, true);
-      break;   
-    case 'b': //to move backwards
-      Motor(E1, M1, pwm_speed, false);
-      Motor(E2, M2, pwm_speed, false);
-      break;     
-    case 'l': //to turn left
-      Motor(E1, M1, pwm_speed, false);
-      Motor(E2, M2, pwm_speed, true);
-      break;   
-    case 'r': //to turn right
-      Motor(E1, M1, pwm_speed, true);
-      Motor(E2, M2, pwm_speed, false);
-      break;  
-    case 's': //full stop
-      Motor(E1, M1, 0, false);
-      Motor(E2, M2, 0, false);
-      break;  
   }
 }
