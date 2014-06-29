ef33afd11c2e1dbea8866916c530522d3c52df85
inusara <timvazquez@gmail.com>
2014-06-29 11:16:07 +0800

    added basic forward motion on start

--- Sketch/PrimeO/PrimeO.ino
+++ Sketch/PrimeO/PrimeO.ino
@@ -1,7 +1,18 @@
+int E1 = 5,
+    M1 = 4,
+    E2 = 6,
+    M2 = 7;
+
 void setup() {
-  
+    pinMode(M1, OUTPUT);
+    pinMode(M2, OUTPUT);
 }
 
 void loop() {
+  analogWrite(E1, 255);
+  analogWrite(E2, 255);
   
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  delay(30);
 }
